d96f74ff70f49e62f08da00ccd807841a4b76ce6
Jesper <jesper_truedsson@hotmail.com>
2020-02-28 11:51:26 +0100

    Have base code to work on now

--- template-app/src/components/card/Card.js
+++ template-app/src/components/card/Card.js
@@ -3,12 +3,18 @@ import React from 'react';
 import '../card/Card.css';
 
 const Card = ({movie}) => {
+
+    let poster = "https://image.tmdb.org/t/p/w500/" + movie.poster_path;
+    let testPoster = "https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg"
+
     return (
         <div className="card">
-            <img src="https://loremflickr.com/320/240" className="img-fluid" />
+            {/* <img src="https://loremflickr.com/320/240" className="img-fluid" /> */}
+            <img src={testPoster} className="img-fluid" />
+            
             <div className="card-body">
-                <h5 className="card-title">{movie.name}</h5>
-                <p className="card-text">{movie.email}</p>
+                <h5 className="card-title">{movie.original_title}</h5>
+                <p className="card-text">{/* {movie.email} */}</p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
--- template-app/src/pages/movies/Movies.js
+++ template-app/src/pages/movies/Movies.js
@@ -15,31 +15,34 @@ const Movies = () => {
   With a state(jsonData) and a method for updating the state(setData) 
   */
   const [jsonData, setData] = useState([]);
-
+  /* https://api.themoviedb.org/3/movie/550?api_key=API_KEY_HERE */
+  /* Url for showing Posters "https://image.tmdb.org/t/p/w500/" + array.poster_path */
   function getMovies() {
-    axios.get('https://jsonplaceholder.typicode.com/users')
-    .then(function (response) {
-      let data = response.data;
-      setData(data);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .finally(function () {
-      // always executed
-    });
-  }
-  getMovies();
+    axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=API_KEY_HERE&language=en-US&page=1')
+      .then(function (response) {
+      
+        console.log(response.data.results)
 
+        /* If i do this i get all data but it loops endlessly? why? */
+        /* const data = response.data.results;
+        setData(data); */
+
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }
+  
 
   // Similar to componentDidMount and componentDidUpdate:
-  /* 
-    useEffect(() => {
-      
-    }); 
-  */
-
+  useEffect(() => {
+    getMovies()
+  }); 
+ 
   /* Traversy Media code */
   /* const [todos, setTodos] = useState([
     {
@@ -56,17 +59,17 @@ const Movies = () => {
 
   return (
     <div className="Movies">
-      <h1>Movies</h1>
+      <h1 className="moviePageTitle">Top 20 Movies</h1>
       <div className="cardGrid">
+        {/* Traversy Media code */}
         {/* { 
           todos.map((todo, index) => (
           <Card key={index} index={index} todo={todo} /> ))
           } */}
-
-          {
-            jsonData.map((movie, index) => (
-            <Card key={index} index={index} movie={movie} /> ))
-          }
+        {
+          jsonData.map((movie, index) => (
+            <Card key={index} index={index} movie={movie} />))
+        }
       </div>
     </div>
   );
